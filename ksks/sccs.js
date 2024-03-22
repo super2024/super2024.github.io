b78e914a40b2e1a77e3577b7db794cda83070e0b
super2024 <163975517+super2024@users.noreply.github.com>
2024-03-22 23:43:06 +0800

    Add files via upload

---
+++ ksks/sccs.js
@@ -0,0 +1,24 @@
+/* function jump1(){
+  window.location.href="https://dncr.gakiurar.com/f/X1k4ygsoLBAm1Mu";
+} */
+/* 改这条是改快手跳 */
+
+
+
+/* function jump11(){
+  window.location.href="https://polaris.zijieapi.com/luckycat/super_inviter/v1/invite_code?bCode=84W4S1&taskId=7140476324040175625";
+} */
+/* 改这条是改抖音跳 */
+
+
+
+for (var a = 0; a < t; a++) {
+  document.getElementsByClassName("aheadr1")[a].href = 'https://xrfl666-1318963746.cos-website.ap-nanjing.myqcloud.com'
+  document.getElementsByClassName("aheadr2")[a].href = 'https://y.gtimg.cn/music/photo_new/T053XD004XLCGI4VrqAi.png'
+ 
+   
+}  
+
+/* aheadr1 完整版后续
+aheadr2 土豪专属
+aheadr3 进群链接 */
